59fef786dfb777c5172bbf5589c29b9064aee8d8
U_35007842 <U_35007842@PC3>
2019-02-13 08:22:31 +0000

    Paso 1 y paso 2

---
+++ src/ets/calculadora.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ets;
+
+
+public class calculadora {
+    
+private int operador1;
+private int operador2;
+private int operacion;
+    
+    public static void main(String[] args) {
+      
+        
+        
+        
+        
+    }
+    
+}
