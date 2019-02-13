5b23a615bd99a3ed4c0c90dccd4ab1f6a4d0f242
U_35007842 <U_35007842@PC3>
2019-02-13 10:19:16 +0000

    Paso 3 y paso 4

--- src/ets/calculadora.java
+++ src/ets/calculadora.java
@@ -9,6 +9,30 @@ package ets;
 public class calculadora {
     
 private int operador1;
+
+    public int getOperador1() {
+        return operador1;
+    }
+
+    public void setOperador1(int operador1) {
+        this.operador1 = operador1;
+    }
+
+    public int getOperador2() {
+        return operador2;
+    }
+
+    public void setOperador2(int operador2) {
+        this.operador2 = operador2;
+    }
+
+    public int getOperacion() {
+        return operacion;
+    }
+
+    public void setOperacion(int operacion) {
+        this.operacion = operacion;
+    }
 private int operador2;
 private int operacion;
     
