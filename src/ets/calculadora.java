0c8bb3115d528008135db93ee1c0301c34eb5bd4
U_35007842 <U_35007842@PC3>
2019-02-13 10:44:50 +0000

    pasos de documentación en la rama master

--- src/ets/calculadora.java
+++ src/ets/calculadora.java
@@ -9,30 +9,50 @@ package ets;
 public class calculadora {
     
 private int operador1;
-
+/**
+ * 
+ * @return variable entera
+ */
     public int getOperador1() {
         return operador1;
     }
-
+/**
+ * 
+ * @param operador1 variable entera
+ */
     public void setOperador1(int operador1) {
         this.operador1 = operador1;
     }
-
+/**
+ * 
+ * @return  variable entera
+ */
     public int getOperador2() {
         return operador2;
     }
-
+/**
+ * 
+ * @param operador2 variable entera
+ */
     public void setOperador2(int operador2) {
         this.operador2 = operador2;
     }
-
+/**
+ * 
+ * @return variable entera 
+ */
     public int getOperacion() {
         return operacion;
     }
+    /**
+     * 
+     * @param operacion variable entera 
+     */
 
     public void setOperacion(int operacion) {
         this.operacion = operacion;
     }
+   
 private int operador2;
 private int operacion;
     
